9a3d1a8a7413c3c46ae9a18d45c5677d3eeb49b2
Martin Burchell <mb2353@cam.ac.uk>
2022-05-24 22:51:20 +0100

    Try to make the bat script fail on error

--- github_action_scripts/python_checks.bat
+++ github_action_scripts/python_checks.bat
@@ -1,14 +1,14 @@
 echo on
 cd %GITHUB_WORKSPACE%
-python -m venv "%USERPROFILE%\venv"
-%USERPROFILE%\venv\Scripts\activate
-python -VV
-python -m site
-python -m pip install -U pip
+python -m venv "%USERPROFILE%\venv" || exit /b %errorlevel%
+%USERPROFILE%\venv\Scripts\activate || exit /b %errorlevel%
+python -VV || exit /b %errorlevel%
+python -m site || exit /b %errorlevel%
+python -m pip install -U pip || exit /b %errorlevel%
 echo dumping pre-installed packages
-python -m pip freeze
+python -m pip freeze || exit /b %errorlevel%
 echo installing pip packages
-python -m pip install -e .
+python -m pip install -e . || exit /b %errorlevel%
 echo running tests
 set CRATE_RUN_WITHOUT_LOCAL_SETTINGS=True
-pytest -v
+pytest -v || exit /b %errorlevel%
