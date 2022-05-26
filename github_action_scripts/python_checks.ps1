dbc996e42b1ab08c19576d1e4dd6fa04798cb6b2
Martin Burchell <mb2353@cam.ac.uk>
2022-05-26 11:35:19 +0100

    Wrong location for installing packages

--- github_action_scripts/python_checks.ps1
+++ github_action_scripts/python_checks.ps1
@@ -5,13 +5,13 @@ python -m venv venv
 python -VV
 python -m site
 python -m pip install -U pip
-echo dumping pre-installed packages
+echo "dumping pre-installed packages"
 python -m pip freeze
-echo installing pip packages
+echo "installing pip packages"
+cd "$env:GITHUB_WORKSPACE"
 python -m pip install -e .
-echo running tests
+echo "running tests"
 $env:CRATE_RUN_WITHOUT_LOCAL_SETTINGS = "True"
 $env:CRATE_ANON_CONFIG = "$env:USERPROFILE\crate_anon_config.ini"
 crate_anon_demo_config > "$env:CRATE_ANON_CONFIG"
-cd "$env:GITHUB_WORKSPACE"
 pytest -v
