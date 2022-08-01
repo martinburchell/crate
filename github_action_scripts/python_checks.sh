65bf56ada2788f2e631b07dab1b9584acac00b7e
Martin Burchell <mb2353@cam.ac.uk>
2022-08-01 16:51:24 +0100

    Add coverage to GitHub action

--- github_action_scripts/python_checks.sh
+++ github_action_scripts/python_checks.sh
@@ -31,4 +31,5 @@ echo checking python for style and errors
 flake8 --config=setup.cfg .
 echo running tests
 export CRATE_RUN_WITHOUT_LOCAL_SETTINGS=True
-pytest -v
+python -m pip install coverage
+pytest -v --cov --cov-report html
